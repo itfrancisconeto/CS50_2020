cbe1d967c1ec27e40ecd8364926411b85995debc
Francisco Neto <it.francisconeto@gmail.com>
2021-06-25 14:31:30 -0300

    change app.py. insert main method.

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def organize():
 
 def main():
      port = int(os.environ.get("PORT", 5000))
-     app.run(host = "0.0.0.0", port = port, debug=True)
+     app.run(host = "0.0.0.0", port = port)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    main()
